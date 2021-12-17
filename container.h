15fe24546f8964d9627607600c4ec6ab70159b73
roooooon <77975244+roooooon@users.noreply.github.com>
2021-12-17 23:55:58 +0300

    Update container.h

--- container.h
+++ container.h
@@ -13,29 +13,30 @@ public:
     enum {max_len = 10000}; // максимальная длина
     int len; // текущая длина
     number cont[max_len];
-};
-
-//------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------
 // Случайный ввод содержимого контейнера
-void con_InRnd(container &con, int size);
+    static void con_InRnd(container &con, int size);
 
 //------------------------------------------------------------------------------
 // Вывод содержимого контейнера в указанный поток
-void con_Out(container &con, ofstream &ofst);
+    static void con_Out(container &con, ofstream &ofst);
 
 //------------------------------------------------------------------------------
 // Ввод содержимого контейнера из указанного потока
-void con_In(container &con, ifstream &ifst);
+    static void con_In(container &con, ifstream &ifst);
 
 //------------------------------------------------------------------------------
 // Очистка контейнера от элементов (освобождение памяти)
-void con_Clear(container &con);
+    static void con_Clear(container &con);
 
 //------------------------------------------------------------------------------
 // Инициализация контейнера
-void Init(container &c);
+    static void Init(container &c);
 
 
-void Binaryinsertion(number mas[], int size);
+    static int binarySearch(int *mas, int size , int value, int start, int end);
+
+    static void Binaryinsertion(int* mas, int size);
+};
 
 #endif //UNTITLED6_CONTAINER_H
