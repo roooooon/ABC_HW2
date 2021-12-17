7d388a559ee8dd705c064958803dae1fb9fd9de9
roooooon <77975244+roooooon@users.noreply.github.com>
2021-12-17 23:56:26 +0300

    Update fraction.cpp

--- fraction.cpp
+++ fraction.cpp
@@ -6,29 +6,26 @@
 #include "fraction.h"
 #include <fstream>
 using namespace std;
-
 //------------------------------------------------------------------------------
 // Вычисление значения дроби
-double fraction_calculation (fraction &f) {
+double fraction::fraction_calculation(fraction &f) {
     return (double)f.numerator / (double)f.denominator;
 }
 
 //------------------------------------------------------------------------------
 // Случайный ввод параметров дроби
-void f_IRnd(fraction &f) {
+void fraction::f_IRnd(fraction &f) {
     f.numerator = Random();
     f.denominator = Random();
 }
-
 //------------------------------------------------------------------------------
 // Ввод параметров дроби из файла
-void fraction_In(fraction &f, ifstream &ifst) {
+void fraction::fraction_In(fraction &f, ifstream &ifst) {
     ifst >> f.numerator >> f.denominator;
 }
-
 //------------------------------------------------------------------------------
 // Вывод параметров дроби в форматируемый поток
-void fraction_Out(fraction &f, ofstream &ofst) {
+void fraction::fraction_Out(fraction &f, ofstream &ofst) {
     ofst << "It is Fraction: numerator = " << f.numerator << ", denominator = " << f.denominator
-         << ". the fraction = " << fraction_calculation(f) << "\n";
+         << ". the fraction = " << fraction::fraction_calculation(f) << "\n";
 }
