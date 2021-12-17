4c90a039d69f4ce6bcc832f6dde5c49fa4ca80ea
roooooon <77975244+roooooon@users.noreply.github.com>
2021-12-17 23:53:47 +0300

    Update complex_number.cpp

--- complex_number.cpp
+++ complex_number.cpp
@@ -1,30 +1,28 @@
 
+
 #include "complex_number.h"
 
 //------------------------------------------------------------------------------
-// Ввод параметров комплексного числа из потока
-void c_In(complex_number &c, ifstream &ifst) {
-    ifst >> c.real >> c.imaginary ;
-}
-
-//------------------------------------------------------------------------------
-// Случайный ввод параметров комплексного числа
-void InRnd(complex_number &c){
-    c.real = Random();
-    c.imaginary = Random();
-}
-
-//------------------------------------------------------------------------------
 // Вычисление значения комплексного числа
-double complex_calculation(complex_number &c) {
+double complex_number::complex_calculation(complex_number &c) {
     return sqrt(c.real * c.real + c.imaginary * c.imaginary);
 }
-
 //------------------------------------------------------------------------------
 // Вывод параметров комплексного числа в файл
-void c_Out(complex_number &c, ofstream &ofst) {
+void complex_number::c_Out(complex_number &c, ofstream &ofst) {
     ofst << "It is complex number: the real = "
          << c.real << ", the imaginary = " << c.imaginary
          << ". complex calculation = " << complex_calculation(c) << "\n";
+}
+//------------------------------------------------------------------------------
+// Случайный ввод параметров комплексного числа
+void complex_number::InRnd(complex_number &c) {
+    c.real = Random();
+    c.imaginary = Random();
+}
+//------------------------------------------------------------------------------
+// Ввод параметров комплексного числа из потока
+void complex_number::c_In(complex_number &c, ifstream &ifst) {
+    ifst >> c.real >> c.imaginary ;
 }
 
