6d2aca32efbb3fdec6ac39c538f7c8fcf07c11a5
roooooon <77975244+roooooon@users.noreply.github.com>
2021-12-17 23:57:08 +0300

    Update fraction.h

--- fraction.h
+++ fraction.h
@@ -7,22 +7,21 @@ using namespace std;
 class fraction {
 public:
     int numerator, denominator;
-};
-
-//------------------------------------------------------------------------------
+    //------------------------------------------------------------------------------
 // Вычисление значения дроби
-double fraction_calculation (fraction &f);
+    static double fraction_calculation (fraction &f);
 
 //------------------------------------------------------------------------------
 // Случайный ввод параметров дроби
-void f_IRnd(fraction &f);
+    static void f_IRnd(fraction &f);
 
 //------------------------------------------------------------------------------
 // Ввод параметров дроби из файла
-void fraction_In(fraction &f, ifstream &ifst);
+    static void fraction_In(fraction &f, ifstream &ifst);
 
 //------------------------------------------------------------------------------
 // Вывод параметров дроби в форматируемый поток
-void fraction_Out(fraction &f, ofstream &ofst);
+    static void fraction_Out(fraction &f, ofstream &ofst);
+};
 
 #endif //UNTITLED6_FRACTION_H
